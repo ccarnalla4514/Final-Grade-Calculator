a5a7bcd62bed1157d5b5a4cd75a6bf3414d13204
Christian Carnalla <ccarnalla4514@stu.d214.org>
2020-12-09 15:16:45 -0600

    Initial Commit

---
+++ Final Grade Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final Grade Calculator
+//
+//  Created by Christian Carnalla on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
